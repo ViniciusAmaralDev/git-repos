811dc39aa30aa95d1752eaf20c77d274b3fd8079
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-28 16:01:01 -0300

    style: adicionado nova cor

--- src/application/theme/index.ts
+++ src/application/theme/index.ts
@@ -4,7 +4,8 @@ const theme = {
     BACKGROUND: "#F9FAFC",
     BLACK: "#37383C",
     RED: "#F37C8E",
-    GRAY: "#B9BDBE",
+    GRAY: "#767D7F",
+    BLUE: "#70A9FF",
     GRAY_LIGHT: "#EAECF1",
   },
   fonts: {
