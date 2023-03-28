5cf431fc5a56cf452a92a7df1e4c3f013f7e5581
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-28 10:30:17 -0300

    feat: adicionado tema

---
+++ src/application/theme/index.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    WHITE: "#FFFFFF",
+    BACKGROUND: "#F9FAFC",
+    BLACK: "#37383C",
+    RED: "#F37C8E",
+    GRAY: "#B9BDBE",
+    GRAY_LIGHT: "#EAECF1",
+  },
+  fonts: {
+    REGULAR: "Montserrat-Regular",
+    MEDIUM: "Montserrat-Medium",
+  },
+};
+
+export default theme;
