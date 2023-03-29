2a0c2feb2f4f98e0be15a6ab6fadbc2dcef6f1c9
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-29 12:05:57 -0300

    refactor: ajustes no hook repository

--- src/application/hooks/repository/index.tsx
+++ src/application/hooks/repository/index.tsx
@@ -3,70 +3,44 @@ import { Repository, RepositoryContext } from "./types";
 import { useNetInfo } from "@react-native-community/netinfo";
 import RepositoryHttpService from "infrastructure/services/repository/http";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { StatusRequestEnum } from "enums/StatusRequestEnum";
+import { useAlert } from "@hooks/alert";
 
 const Context = createContext({} as RepositoryContext);
 
 export const RepositoryProvider = ({ children }: Children) => {
+  const { showAlert } = useAlert();
   const { isConnected } = useNetInfo();
   const httpService = new RepositoryHttpService();
 
+  const [statusRequest, setStatusRequest] = useState(
+    StatusRequestEnum.STAND_BY
+  );
+
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
-  const getRepositories = async () => {
+  const getRepositories = async (owner: string) => {
+    setStatusRequest(StatusRequestEnum.PENDING);
     try {
-      const { data } = await httpService.getRepositories("viniciusamaraldev");
-      setRepositories(data);
+      const { data } = await httpService.getRepositories(owner);
+      const filteredRepositories = repositories.filter(
+        (repo) => repo.owner.login !== owner
+      );
+      setRepositories([...filteredRepositories, ...data]);
+      setStatusRequest(StatusRequestEnum.DONE);
     } catch (error: any) {
-      throw error;
+      setStatusRequest(StatusRequestEnum.ERROR);
+      showAlert({
+        message: "Não foi possível obter os repositórios deste usuário",
+      });
     }
   };
 
-  useEffect(() => {
-    // getRepositories();
-  }, []);
-
   return (
-    <Context.Provider value={{ repositories }}>{children}</Context.Provider>
+    <Context.Provider value={{ repositories, statusRequest, getRepositories }}>
+      {children}
+    </Context.Provider>
   );
 };
 
 export const useRepository = () => useContext(Context);
-
-const a = {
-  avatar_url: "https://avatars.githubusercontent.com/u/93560235?v=4",
-  bio: "Desenvolvedor React Native",
-  blog: "",
-  company: "Bov Control",
-  created_at: "2021-11-02T00:51:41Z",
-  email: null,
-  events_url: "https://api.github.com/users/ViniciusAmaralDev/events{/privacy}",
-  followers: 1,
-  followers_url: "https://api.github.com/users/ViniciusAmaralDev/followers",
-  following: 3,
-  following_url:
-    "https://api.github.com/users/ViniciusAmaralDev/following{/other_user}",
-  gists_url: "https://api.github.com/users/ViniciusAmaralDev/gists{/gist_id}",
-  gravatar_id: "",
-  hireable: null,
-  html_url: "https://github.com/ViniciusAmaralDev",
-  id: 93560235,
-  location: "São Luís - MA",
-  login: "ViniciusAmaralDev",
-  name: "Vinicius Amaral",
-  node_id: "U_kgDOBZOdqw",
-  organizations_url: "https://api.github.com/users/ViniciusAmaralDev/orgs",
-  public_gists: 0,
-  public_repos: 4,
-  received_events_url:
-    "https://api.github.com/users/ViniciusAmaralDev/received_events",
-  repos_url: "https://api.github.com/users/ViniciusAmaralDev/repos",
-  site_admin: false,
-  starred_url:
-    "https://api.github.com/users/ViniciusAmaralDev/starred{/owner}{/repo}",
-  subscriptions_url:
-    "https://api.github.com/users/ViniciusAmaralDev/subscriptions",
-  twitter_username: null,
-  type: "User",
-  updated_at: "2023-01-14T01:34:47Z",
-  url: "https://api.github.com/users/ViniciusAmaralDev",
-};
