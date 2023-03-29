cdac6c6a059f461ef70377c5a8e5ccecf322ea17
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-29 12:02:42 -0300

    feat: adicionado hook de alerta

--- src/application/hooks/owner/index.tsx
+++ src/application/hooks/owner/index.tsx
@@ -1,27 +1,30 @@
 import { Owner, OwnerContext } from "./types";
 import { Children } from "../../../@types/Children";
 import OwnerHttpService from "infrastructure/services/owner/http";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { useAlert } from "@hooks/alert";
 
 const Context = createContext({} as OwnerContext);
 
 export const OwnerProvider = ({ children }: Children) => {
+  const { showAlert } = useAlert();
   const httpService = new OwnerHttpService();
   const [owners, setOwners] = useState<Owner[]>([]);
 
   const getOwner = async (owner: string) => {
     try {
       const { data } = await httpService.getOwner(owner);
-      setOwners((values) => [...values, data]);
+      const filtered = owners.filter((value) => value.login !== owner);
+      setOwners([...filtered, data]);
     } catch (error: any) {
-      console.log("ERROR =>", error);
-      throw error;
+      showAlert({
+        message:
+          error.response.status === 404
+            ? "Este usuário não existe!"
+            : "Houve um problema, tente novamente mais tarde!",
+      });
     }
   };
-
-  useEffect(() => {
-    getOwner("viniciusamaraldev");
-  }, []);
 
   return (
     <Context.Provider value={{ owners, getOwner }}>{children}</Context.Provider>
