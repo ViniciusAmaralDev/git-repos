75442fbc3722ca6b6ea691851aefd5536f675e31
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-29 12:05:04 -0300

    feat: adicionado serviços de owner e repository

--- src/infrastructure/services/owner/http/index.ts
+++ src/infrastructure/services/owner/http/index.ts
@@ -1,8 +1,10 @@
+import { Owner } from "@hooks/owner/types";
+import { AxiosPromise } from "axios";
 import GitHubApi from "infrastructure/http/GitHubApi";
 import IOwnerHttpService from "./interfaces/IOwnerHttpService";
 
 export default class OwnerHttpService implements IOwnerHttpService {
-  public async getOwner(owner: string) {
+  public async getOwner(owner: string): AxiosPromise<Owner> {
     return GitHubApi.get(`/users/${owner}`);
   }
 }
--- src/infrastructure/services/repository/http/index.ts
+++ src/infrastructure/services/repository/http/index.ts
@@ -1,8 +1,12 @@
+import { Repository } from "@hooks/repository/types";
+import { AxiosPromise } from "axios";
 import GitHubApi from "infrastructure/http/GitHubApi";
 import IRepositoryHttpService from "./interfaces/IRepositoryHttpService";
 
 export default class RepositoryHttpService implements IRepositoryHttpService {
-  public async getRepositories(repositoryOwner: string) {
-    return GitHubApi.get(`/users/${repositoryOwner}`);
+  public async getRepositories(
+    repositoryOwner: string
+  ): AxiosPromise<Repository[]> {
+    return GitHubApi.get(`/users/${repositoryOwner}/repos`);
   }
 }
