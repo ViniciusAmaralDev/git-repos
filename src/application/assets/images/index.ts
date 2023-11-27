62eaae6b6b64b1566c6465d9d8943cee3c60a686
Vinicius Amaral <dev.viniciusamaral@gmail.com>
2023-11-27 12:04:00 -0300

    feat: adicionado assets

---
+++ src/application/assets/images/index.ts
@@ -0,0 +1,6 @@
+import iconAppBlack from "./app-icon-black-512.png";
+import iconAppWhite from "./app-icon-white-512.png";
+
+export const images = {
+  appIcon: { dark: iconAppBlack, light: iconAppWhite },
+};
