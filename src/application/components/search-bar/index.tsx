530c926292839e008ca5fadad2cb896f6550d352
Vinicius Amaral <dev.viniciusamaral@gmail.com>
2023-11-30 10:36:25 -0300

    feat: adicionado componente searchbar

--- src/application/components/search-bar/index.tsx
+++ src/application/components/search-bar/index.tsx
@@ -1,25 +1,36 @@
 import { Input } from "../base/input";
 import React, { forwardRef, useRef, useState } from "react";
 import { IconButton } from "../icon-button";
-import { CloseIcon, Container, SearchIcon } from "./styles";
+import { CloseIcon, Container, SearchButton, SearchIcon } from "./styles";
 import { StyleProp, TextInput, TextInputProps, ViewStyle } from "react-native";
+import { useTheme } from "@/application/contexts/ThemeContext";
 
 interface SearchBarProps extends TextInputProps {
   containerStyle?: StyleProp<ViewStyle>;
   clear: () => void;
+  onSearch: () => void;
 }
 
 export const SearchBar = forwardRef<any, SearchBarProps>(
-  ({ value, containerStyle, clear, ...rest }, ref) => {
+  ({ value, containerStyle, clear, onSearch, ...rest }, ref) => {
+    const { theme } = useTheme();
+    const isEmpty = value?.length === 0;
+
+    const iconColor = !isEmpty ? theme.colors.text : theme.colors.border;
+
     return (
       <Container style={containerStyle}>
-        <SearchIcon name="search" />
+        {!isEmpty && (
+          <IconButton icon={<CloseIcon name="close" onPress={clear} />} />
+        )}
 
         <Input ref={ref} {...rest} />
 
-        {value?.length > 0 && (
-          <IconButton icon={<CloseIcon name="close" onPress={clear} />} />
-        )}
+        <SearchButton
+          isActive={!isEmpty}
+          icon={<SearchIcon name="search" color={iconColor} />}
+          onPress={onSearch}
+        />
       </Container>
     );
   }
--- src/application/components/search-bar/styles.ts
+++ src/application/components/search-bar/styles.ts
@@ -1,10 +1,15 @@
-import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { IconButton } from "../icon-button";
 import styled from "styled-components/native";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+
+interface SearchButtonProps {
+  isActive?: boolean;
+}
 
 export const Container = styled.View`
   gap: 16px;
   height: 40px;
-  padding: 0px 16px;
+  padding-left: 16px;
   border-radius: 8px;
   flex-direction: row;
   align-items: center;
@@ -12,9 +17,19 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.border};
 `;
 
+export const SearchButton = styled(IconButton)<SearchButtonProps>`
+  height: 100%;
+  padding: 8px 12px;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  background-color: ${({ theme, isActive }) =>
+    isActive ? theme.colors.secondary : theme.colors.placeholder};
+`;
+
 export const SearchIcon = styled(FontAwesome).attrs(({ theme }) => ({
   size: 20,
-  color: theme.colors.text,
 }))``;
 
 export const CloseIcon = styled(AntDesign).attrs(({ theme }) => ({
