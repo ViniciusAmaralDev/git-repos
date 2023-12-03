2052349a2e8e782cbeca0d0b193da8c4dedef64b
Vinicius Amaral <dev.viniciusamaral@gmail.com>
2023-12-03 11:54:24 -0300

    style: atualizado estilo

--- src/application/components/base/image/styles.ts
+++ src/application/components/base/image/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 
 export const ImageBackground = styled.ImageBackground`
+  align-items: center;
+  justify-content: center;
   background-color: ${({ theme }) => theme.colors.border};
 `;
 
